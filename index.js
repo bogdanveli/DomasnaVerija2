abbd09f41af4228868f13783ce95eecd6e27ed03
Bogdan Velichkovski <bogdance98@gmail.com>
2020-02-06 21:33:29 +0100

    Commit numero uno

---
+++ index.js
@@ -0,0 +1,90 @@
+var fs = require('fs');
+fs.readFile("lorem.txt", 'utf8', function(err, data) {
+    if(err){
+        console.log('could not read the lorem file');
+        return;
+    }
+    function brojRec()  //BROJ NA RECENICI
+    {
+    let str = data;
+    let str1 = str.split(".");
+    let recenici = str1.length;
+    console.log("We have " + recenici + " sentences");
+    }
+
+    function brojZborovi(){ //BROJ NA ZBOROVI
+        let str = data;
+    let str1 = str.split(" ");
+    let zborovi = str1.length;
+    console.log("We have " + zborovi + " words");
+    }
+
+    function brojKarakteri(){ // BROJ NA KARAKTERI
+        let str = data;
+    let str1 = str.split("");
+    let karakteri = str1.length;
+    console.log("We have " + karakteri + " characters");
+    }
+
+    function lessThen7(){ // Zborovi POMALI od 7
+    let count=0;    
+    let str = data;
+    let str1 = str.split(" ");
+    for(let i=0; i<str1.length; i++){
+        if(str1[i].length<7){
+            count++;
+        }
+    }
+    console.log("We have " + count + " words less then 7 characters");
+    }
+
+    function GreaterThen7(){ // Zborovi Pogolemi od 7
+        let count=0;    
+        let str = data;
+        let str1 = str.split(" ");
+        for(let i=0; i<str1.length; i++){
+            if(str1[i].length>7){
+                count++;
+            }
+        }
+        console.log("We have " + count + " words greater then 7 characters");
+        }
+
+        function Egual7(){ // Zborovi so golemina 7 chars
+            let count=0;    
+            let str = data;
+            let str1 = str.split(" ");
+            for(let i=0; i<str1.length; i++){
+                if(str1[i].length==7){
+                    count++;
+                }
+            }
+            console.log("We have " + count + " words equal to 7 chars");
+            }
+    
+        function MostUsedWord(){   //Most common word in the file
+              
+            let str = data;
+            let str1 = str.split(" ");
+            count="";
+            for(let i=0; i<str1.length; i++){
+                for(let j=0; j<str1.length; j++ ){
+                    if(str1[i]==str1[j]){
+                        count=str1[i][j];
+                        break;
+                    }
+                }
+            }
+            console.log("We have the word " + count + " is the most used in the txt file.");
+            }
+        
+
+    brojRec();
+    brojZborovi();
+    brojKarakteri();
+    lessThen7();
+    GreaterThen7();
+    Egual7();
+    MostUsedWord();
+});
+
